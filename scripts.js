af80ab77e2cad0210c8bb0fc50ece408bd90bb0f
H-Abdullah <abdullah.ebn.hasan@gmail.com>
2023-05-21 17:02:44 +0800

    v0.1: Finish base code for html and css, and also add simple logic for js(40%).

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const buttons = document.getElementsByClassName('button');
+const topOutput = document.getElementById('output-screen-top');
+const bottomOutput = document.getElementById('output-screen-bottom');
+const clears = document.getElementsByClassName('clear');
+const operations = document.getElementsByClassName('operation');
+const numbers = document.getElementsByClassName('number');
+
+function clearSomething() {
+  console.log('You have clicked any clear buttons');
+}
+
+function operand() {
+  console.log('You have clicked any operation buttons');
+}
+
+function inputNumbers() {
+  console.log('You have clicked any number buttons');
+}
+
+Array.from(clears).forEach(clear => {
+  clear.addEventListener('click', clearSomething());
+});
+
+Array.from(operations).forEach(operation => {
+  operation.addEventListener('click', operand());
+});
+
+Array.from(numbers).forEach(number => {
+  number.addEventListener('click', inputNumbers());
+});
