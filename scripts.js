8cc880947c46a7ef80259d6fc617df2b27392537
H-Abdullah <abdullah.ebn.hasan@gmail.com>
2023-05-25 16:08:45 +0800

    v0.8: - Add support logic for all calculation inside functions - Add comments for all possible codes - Refactor html and css code for more readability and good design - Remove some buttons: '+/-', '%', 'x2' and  'x!' just to keep the calculator a simple as possible - Add interval function that always check for max limit input
    Known bugs:
    - 0 / 0 = NaN. By all means, zero inputs for first number already prevented but you can get 0 by doing operations with other numbers
    - Can't change operation symbols for first inputted operator
    - Can input numbers even after max limit message evoked
    - Top screen easily to reach max limit(this might not be bug, more like need to refactor code)

--- scripts.js
+++ scripts.js
@@ -1,21 +1,31 @@
 const buttons = document.getElementsByClassName('button');
 const topOutput = document.getElementById('output-screen-top');
 const bottomOutput = document.getElementById('output-screen-bottom');
+const outputContainer = document.getElementById('output-container');
 const clears = document.getElementsByClassName('clear');
 const operations = document.getElementsByClassName('operation');
 const numbers = document.getElementsByClassName('number');
 
-let firstNum = '';
-let secondNum = '';
-let operator = '';
-// Clear bottomOutput once for the inputNumbers() second half.
-let isCleared = false; 
-// Ensure that only one operator entered at time
-let opCount = 0; 
-let lastOp = ''; // Hold past Op to replace in replaceOperator()
-let calcOp = ''; // Use for calcResult(), holding last operation before changed
+let firstNum = ''; // Hold number for the first group of numbers
+let secondNum = ''; // Hold number for the second group of numbers
+let operator = ''; // Hold the operator that used for calculation
+let isCleared = false; // Clear bottomOutput once for the inputNumbers() second-half
+let opCount = 0; // Ensure that only one operator entered at time
+let lastOp = ''; // Hold past operator to replace in replaceOperator()
+let calcOp = ''; // Used for calcResult(), holding last operation before changed
+let pointCount = 0; // Ensure no second point '.' entered after the first one
 
-function calcResult(calcOp) {
+function updateTopScreen(strNum) { // Refresh and update top screen
+  return topOutput.innerText += strNum;
+}
+function updateBottomScreen(strNum) { // Refresh and update bottom screen
+  return bottomOutput.innerText += strNum;
+}
+function clearScreen(output) { // Clear screen from previous values
+  return output.innerText = '';
+}
+
+function calcResult(calcOp) { // Calculate value that provided in *num variables
   firstNum = parseInt(firstNum);
   secondNum = parseInt(secondNum);
   switch (calcOp) {
@@ -42,33 +52,49 @@ function calcResult(calcOp) {
   }
 }
 
-function allClear() { // Resetting everything
+function allClear() { // Reset everything
   firstNum = '';
   secondNum = '';
   operator = '';
   isCleared = false;
   opCount = 0;
   lastOp = '';
-  topOutput.innerText = '';
-  bottomOutput.innerText = '';
+  calcOp = '';
+  pointCount = 0;
+  topOutput.style.removeProperty('font-size'); // Remove added properties from maxLimitResetCalc() 
+  topOutput.style.removeProperty('color'); // Remove added properties from maxLimitResetCalc()
+  bottomOutput.style.removeProperty('font-size'); // Remove added properties from maxLimitResetCalc()
+  bottomOutput.style.removeProperty('color'); // Remove added properties from maxLimitResetCalc()
+  clearScreen(topOutput);
+  clearScreen(bottomOutput);
 }
 
-function clearOnly() {
-  if (firstNum === '' && secondNum === '') return;
-  if (firstNum !== '' && secondNum === '') {
+function clearEntry() { // Clear last number entries
+  if (firstNum === '' && secondNum === '') return; // Nothing to delete if nothing to delete
+  // Delete last inputted numbers from firstNum until an operator is inputted  
+  if (firstNum !== '' && secondNum === '' && operator === '') { 
     firstNum = '';
-    topOutput.innerText = '';
-    bottomOutput.innerText = '';
+    clearScreen(topOutput);
+    clearScreen(bottomOutput);
   }
-  // if (firstNum !== '' && second !== '') {
-  //   secondNum = '';
-  //   bottomOutput.innerText = '';
-  // }
+
+  // Delete last inputted numbers from secondNum until an operator is inputted  
+  if (firstNum !== '' && secondNum !== '') { 
+    let clearLastInput = topOutput.innerText
+      .split('').reverse().join('')
+      .replace(secondNum.split('').reverse().join(''), '')
+      .split('').reverse().join('');
+    secondNum = '';
+    topOutput.innerText = clearLastInput;
+    clearScreen(bottomOutput);
+  }
 }
 
-function replaceOperator(event) {
+// Change last inputted operators flexibly (except for the first operator <-- this is bug)
+function replaceOperator(event) { 
   let newOp = event.target.innerText;
-  let replaceOp = topOutput.innerText.split('').reverse().join('')
+  let replaceOp = topOutput.innerText
+    .split('').reverse().join('')
     .replace(lastOp, newOp)
     .split('').reverse().join('');
   topOutput.innerText = replaceOp;
@@ -76,70 +102,89 @@ function replaceOperator(event) {
   lastOp = newOp;
 }
 
-function clearSomething(event) {
+// Function that triggered if any of 'AC' or 'C' buttons clicked
+function clearSomething(event) { 
   let btn = event.target.innerText;
   switch (btn) {
     case 'AC':
       allClear();
       break;
-    case 'C':
-      clearOnly();
+    case 'CE':
+      clearEntry();
       break;
   }
 }
 
+// Create and hold operator symbol to be used later
 function operand(event) {
   let numOperator = event.target.innerText;
-  if (topOutput.innerText === '') return; // No operators as first input
+  // No operators as first input in firstNum and secondNum
+  if (secondNum === '' && (firstNum === '' || operator !== '')) return; 
+  // If equal '=' inputted, no other button can be clicked except 'AC' button
   if (topOutput.innerText === '\75') {
-    calcResult();
-    return;
+    return calcResult();
   }
-  if (opCount === 1) { // Change operator
+  
+  if (opCount === 1 && operator !== '') { // Change operators flexibly
     operator = numOperator;
     replaceOperator(event);
-  }
-
-  if (operator === '' || opCount === 0) { 
+  } else { 
+    // Original conditional before refactored:
+    // if (opCount === 0 || operator === 0);
     operator = numOperator;
     lastOp = numOperator;
     topOutput.innerText += numOperator;
     isCleared = false;
     opCount = 1;
-    if (secondNum === '') {
-      return;
-    } else {
+    pointCount = 0;
+    if (secondNum !== '') {
       calcResult(calcOp);
-    }
+    } 
   }
 }
 
+// Get numbers from buttons and keep in *num variables
 function inputNumbers(event) {
   let strNum = event.target.innerText;
 
-  // Ensure no number input after equal '='
+  // Prevent any number inputted after equal '='
   if (lastOp !== '\75') {
 
-    // Ensure no '0' or '00' inputted as first num
-    if (operator === '' && firstNum === '' 
+    // Prevent any '0','00' and '.' inputted as first num in firstNum
+    if ((operator === '' && firstNum === '') 
       && (strNum === '0' || strNum === '00' || strNum === '.')) 
       return;
 
-    // Ensure any numbers (except zeros) can be inputted as first num
-    if (operator === '') {
+    // Prevent any '0','00' and '.' inputted as first num in secondNum
+    if ((operator !== '' && secondNum === '') 
+      && (strNum === '0' || strNum === '00' || strNum === '.')) 
+      return;
+
+    // Prevent any '.' twice inputted after initial '.' inputted
+    if (operator !== '' && strNum === '.' && secondNum.includes('.'))
+      return;
+
+    // Input any numbers (except zeros) as first number in firstNum
+    if (operator === '' && strNum === '.' && pointCount === 0) {
       firstNum += strNum;
       topOutput.innerText += strNum;
       bottomOutput.innerText += strNum;
-    }  
+      pointCount = 1;
+    } else if (operator === '' && strNum !== '.') {
+      firstNum += strNum;
+      topOutput.innerText += strNum;
+      bottomOutput.innerText += strNum;
+    }
 
     // Switch to secondNum after firstNum is filled
     if (operator !== '' && isCleared === false) {
-      bottomOutput.innerText = '';
+      clearScreen(bottomOutput); // One-time clear to update the screen
       secondNum = strNum;
       topOutput.innerText += strNum;
       bottomOutput.innerText += strNum;
       isCleared =  true;
       opCount = 0;
+      pointCount = 1;
       calcOp = lastOp;
     } else if (operator !== '' && isCleared === true) {
       secondNum += strNum;
@@ -150,14 +195,43 @@ function inputNumbers(event) {
   return;
 }
 
+// Reset calculator if max input reached
+function maxLimitResetCalc(output) {
+  output.style.setProperty('font-size', '0.8em');
+  output.style.setProperty('color', 'red');
+  output.innerText = '<Max Input: Resetting in five seconds..>';
+  setTimeout(allClear, 5000);
+}
+
+// Check max screen input
+function checkScreenOverflow() {
+  setInterval(() => {
+    const isTopOverflow = topOutput.scrollWidth > outputContainer.clientWidth;
+    const isBottomOverflow = bottomOutput.scrollWidth > outputContainer.clientWidth;
+    if (isTopOverflow) {
+      maxLimitResetCalc(topOutput);
+    } else if (isBottomOverflow) {
+      maxLimitResetCalc(bottomOutput);
+    } else {
+      return;
+    }
+  }, 100);
+}
+
+// Add event listener for all red buttons
 Array.from(clears).forEach(clear => {
   clear.addEventListener('click', clearSomething);
 })
 
+// Add event listener for all orange buttons
 Array.from(operations).forEach(operation => {
   operation.addEventListener('click', operand);
 })
 
+// Add event listener for all white buttons
 Array.from(numbers).forEach(number => {
   number.addEventListener('click', inputNumbers);
 })
+
+// Interval check for max output screen
+checkScreenOverflow();
